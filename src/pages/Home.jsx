ad39250da3cf0a232fba5c961f1eaabf2f4f3cb9
hemantjoshee <hemant1341.becse24@chitkara.edu.in>
2026-03-01 21:38:55 +0530

    pageTitleChange

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,12 @@ import WhyChooseSection from "../Components/WhyChooseSection";
 import ClientSolutions from "../Components/ClientSolutions";
 import Testimonials from "../Components/Testimonials";
 import Footer from "../Components/Footer";
+import ParticleBackground from "../Components/ParticleBackground";
 
 const Home = () => {
   return (
     <>
+      <ParticleBackground />
       <Navbar />
       <ClientSolutions />
       <WhyChooseSection />
